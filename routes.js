ce3fecdcc4bde889bfbf84a09efb05f01b53f5cc
arielmell0 <arielmello@rede.ulbra.br>
2022-02-20 23:11:09 -0300

    feat: listando todas as pessoas / listando uma unica pessoa com a url do id

--- routes.js
+++ routes.js
@@ -4,8 +4,12 @@ const route = express.Router()
 const HomeController = require('./src/Controller/HomeController')
 const PessoaController = require('./src/Controller/PessoaController')
 
+// Rota inicial (boas vindas)
 route.get('/', HomeController.home)
 
-route.post('/pessoa', PessoaController.pessoa)
+route.get('/pessoa', PessoaController.listaPessoas)
+route.get('/pessoa/:id', PessoaController.listaUmaPessoa)
+
+route.post('/pessoa', PessoaController.cadastroPessoa)
 
 module.exports = route
--- src/Controller/PessoaController.js
+++ src/Controller/PessoaController.js
@@ -1,6 +1,8 @@
+const Pessoa = require('../Models/PessoaModel')
 const PessoaModel = require('../Models/PessoaModel')
 
-exports.pessoa = async(req, res) => {
+// Create
+exports.cadastroPessoa = async(req, res) => {
     const { nome, salario, aprovado } = req.body
 
     if(!nome) {
@@ -19,5 +21,29 @@ exports.pessoa = async(req, res) => {
         res.status(201).json({ message: 'Pessoa inserida no sistema com sucesso!'})
     } catch(error) {
         res.status(500).json( { erro: error } )
+    }
+}
+
+// Read
+exports.listaPessoas = async(req, res) => {
+    try {
+        const pessoa = await PessoaModel.find()
+
+        res.status(200).json(pessoa)
+    } catch(error) {
+        res.status(500).json({ erro: error })
+    }
+}
+
+exports.listaUmaPessoa = async(req, res) => {
+    // extrair o dado da requisição pela url = req.params
+    const id = req.params.id
+
+    try {
+        const pessoa = await PessoaModel.findOne({ _id: id })
+        
+        res.status(200).json(pessoa)
+    } catch(error) {
+        res.status(500).json({ erro: error})
     }
 }
