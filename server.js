84acf69024541113333c9dae640b2f46af7abf01
arielmell0 <arielmello@rede.ulbra.br>
2022-03-16 20:10:30 -0300

    feat: definindo a porta para o deploy

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
-const port = 3030
+const port = process.env.PORT || 3030
 
 const routes = require('./routes')
 
