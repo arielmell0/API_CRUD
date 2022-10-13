a681c3ef6594ca27e0a102f6e4e6dc297b0f32a2
arielmell0 <arieldemello@hotmail.com>
2022-10-13 17:44:03 -0300

    feat: testing the GET '/pessoa' route
    this is a test.todo, the test is not ready yet

--- src/server.js
+++ src/server.js
@@ -1,8 +1,16 @@
 require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
 const routes = require('./routes')
+
+mongoose.connect(process.env.CONNECTIONSTRING)
+    .then(() => {
+        console.log('Banco de dados conectado.')
+        app.emit('bancoOn')
+    })
+    .catch(erro => console.log('Ops ocorreu um erro, código: ', erro))
 
 // forma de ler JSON /middlewares
 app.use(express.urlencoded({
--- src/test/e2e/api.test.js
+++ src/test/e2e/api.test.js
@@ -10,4 +10,17 @@ describe('API E2E Test Suite', () => {
         expect(data).toBeInstanceOf(Object)
         expect(data.message).toBe('Bem vindo a API de CRUD')
     })
+    
+    test.todo('GET /pessoa - should return an array with a person id, name, salary and situation', async () => {
+        const response = await superTest(Server)
+        .get('/pessoa')
+        const data = JSON.parse(response.text)
+
+        expect(data).toBeInstanceOf(Object)
+
+        expect(data[0]._id).toBeInstanceOf(String)
+        expect(data[0].nome).toBeInstanceOf(String)
+        expect(data[0].salario).toBeInstanceOf(Number)
+        expect(data[0].aprovado).toBeInstanceOf(Boolean)
+    })
 })
