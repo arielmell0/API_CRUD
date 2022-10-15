01c54671e93e9651cf32a9ef5fce3417014b6b56
arielmell0 <arielmello@rede.ulbra.br>
2022-10-15 01:06:13 -0300

    feat: test GET '/pessoa' working correctly
    verify if the route GET '/pessoa' return an array with the specified values

--- src/test/e2e/api.test.js
+++ src/test/e2e/api.test.js
@@ -11,16 +11,16 @@ describe('API E2E Test Suite', () => {
         expect(data.message).toBe('Bem vindo a API de CRUD')
     })
     
-    test.todo('GET /pessoa - should return an array with a person id, name, salary and situation', async () => {
+    test('GET /pessoa - should return an array with a person id, name, salary and situation', async () => {
         const response = await superTest(Server)
         .get('/pessoa')
         const data = JSON.parse(response.text)
 
         expect(data).toBeInstanceOf(Object)
 
-        expect(data[0]._id).toBeInstanceOf(String)
-        expect(data[0].nome).toBeInstanceOf(String)
-        expect(data[0].salario).toBeInstanceOf(Number)
-        expect(data[0].aprovado).toBeInstanceOf(Boolean)
+        expect(typeof data[0]._id).toBe('string')
+        expect(typeof data[0].nome).toBe('string')
+        expect(typeof data[0].salario).toBe('number')
+        expect(typeof data[0].aprovado).toBe('boolean')
     })
 })
