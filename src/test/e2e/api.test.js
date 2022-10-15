74c531c0ca90323cc4ab09614a11eba842761b58
arielmell0 <arieldemello@hotmail.com>
2022-10-15 10:14:45 -0300

    feat: add new test GET '/pessoa/:id'
    the test should return a object with a unique person and the status code 200

--- src/test/e2e/api.test.js
+++ src/test/e2e/api.test.js
@@ -23,4 +23,32 @@ describe('API E2E Test Suite', () => {
         expect(typeof data[0].salario).toBe('number')
         expect(typeof data[0].aprovado).toBe('boolean')
     })
+
+    test('GET /pessoa/id - should return a object with a unique person and status 200', async () => {
+        const personId = '6345b24ed4e74c38e2303edc'
+
+        const response = await superTest(Server)
+        .get(`/pessoa/${personId}`)
+
+        const data = JSON.parse(response.text)
+
+        expect(data).toBeInstanceOf(Object)
+        expect(data._id).toBe('6345b24ed4e74c38e2303edc')
+        expect(response.status).toBe(200)
+    })
+
+    test('POST /pessoa - should save an item and return status 200 with a message of sucess', async () => {
+        const response = await superTest(Server)
+        .post('/pessoa')
+        .send({
+            nome: 'João',
+            salario: 1375,
+            aprovado: false
+        })
+
+        const data = response.text.trim()
+
+        expect(response.status).toBe(201)
+        expect(data).toBe('{"message":"Pessoa inserida no sistema com sucesso!"}')
+    })
 })
